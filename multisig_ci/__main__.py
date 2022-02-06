78faefa5bda057aa26ee723e3b16865bfcaf3d93
kx9x <kx9x@protonmail.com>
2022-02-05 19:23:53 -0800

    feat: v0.0.2 add support for hydrate_compiler_cache

--- multisig_ci/__main__.py
+++ multisig_ci/__main__.py
@@ -1,5 +1,10 @@
 from multisig_ci.run_brownie import run_brownie
+from multisig_ci.hydrate_ci_cache import hydrate_compiler_cache
 import sys
 
 if __name__ == '__main__':
+    if 'hydrate_compiler_cache' in sys.argv:
+        hydrate_compiler_cache()
+        sys.exit(0)
+
     run_brownie(sys.argv[1:])
