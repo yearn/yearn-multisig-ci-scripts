cdbb90c87c6997e93c6d80c1948a3fd119f8816a
kx9x <kx9x@protonmail.com>
2022-11-09 19:35:31 -0800

    fix: add tenderly fork as a new option in @sign

--- multisig_ci/sign.py
+++ multisig_ci/sign.py
@@ -1,9 +1,25 @@
 from multisig_ci.safes import safe
 
+def _tenderly_fork(safe):
+   import requests
+   import brownie
 
-def sign(nonce_arg = None, skip_preview = False, post_tx = False):
+   fork_base_url = "https://simulate.yearn.network/fork"
+   payload = {"network_id": "1"}
+   resp = requests.post(fork_base_url, headers={}, json=payload)
+   fork_id = resp.json()["simulation_fork"]["id"]
+   fork_rpc_url = f"https://rpc.tenderly.co/fork/{fork_id}"
+   print(fork_rpc_url)
+   tenderly_provider = brownie.web3.HTTPProvider(fork_rpc_url, {"timeout": 600})
+   brownie.web3.provider = tenderly_provider
+   safe.w3.provider = tenderly_provider
+   print(f"https://dashboard.tenderly.co/yearn/yearn-web/fork/{fork_id}")
+
+def sign(nonce_arg = None, skip_preview = False, post_tx = False, tenderly_fork = False):
     def _sign(func):
         def wrapper():
+            if tenderly_fork:
+                _tenderly_fork(safe)
             func()
             safe_tx = safe.multisend_from_receipts(safe_nonce=nonce)
             if not skip_preview:
