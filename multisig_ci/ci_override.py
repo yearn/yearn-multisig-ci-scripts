c0ff499a26d69f982d4472f2d56536e5e5b3968c
kx9x <kx9x@protonmail.com>
2023-07-26 21:00:26 -0700

    fix: remove yearn gnosis backend code

--- multisig_ci/ci_override.py
+++ multisig_ci/ci_override.py
@@ -18,25 +18,15 @@ DELEGATE_ADDRESS = os.environ.get("DELEGATE_ADDRESS")
 home_directory = os.environ.get("HOME")
 
 gnosis_frontend_urls = {
-    'gnosis': {
-        1: 'https://app.safe.global/eth:{0}/transactions/queue',
-        4: 'https://app.safe.global/rin:{0}/transactions/queue',
-        5: 'https://app.safe.global/gor:{0}/transactions/queue',
-        10: 'https://app.safe.global/oeth:{0}/transactions/queue',
-        56: 'https://app.safe.global/bsc:{0}/transactions/queue',
-        100: 'https://app.safe.global/xdai:{0}/transactions/queue',
-        137: 'https://app.safe.global/matic:{0}/transactions/queue',
-        250: 'https://safe.fantom.network/ftm:{0}/transactions/queue',
-        42161: 'https://app.safe.global/arb1:{0}/transactions/queue'
-    },
-    'yearn': {
-        1: 'http://mainnet.gnosis.yearn.tools/eth:{0}/transactions/queue',
-        4: 'http://mainnet.gnosis.yearn.tools/rin:{0}/transactions/queue',
-        56: 'http://mainnet.gnosis.yearn.tools/bsc:{0}/transactions/queue',
-        100: 'http://mainnet.gnosis.yearn.tools/xdai:{0}/transactions/queue',
-        137: 'http://mainnet.gnosis.yearn.tools/matic:{0}/transactions/queue',
-        42161: 'http://mainnet.gnosis.yearn.tools/arb1:{0}/transactions/queue'
-    }
+    1: 'https://app.safe.global/eth:{0}/transactions/queue',
+    4: 'https://app.safe.global/rin:{0}/transactions/queue',
+    5: 'https://app.safe.global/gor:{0}/transactions/queue',
+    10: 'https://app.safe.global/oeth:{0}/transactions/queue',
+    56: 'https://app.safe.global/bsc:{0}/transactions/queue',
+    100: 'https://app.safe.global/xdai:{0}/transactions/queue',
+    137: 'https://app.safe.global/matic:{0}/transactions/queue',
+    250: 'https://safe.fantom.network/ftm:{0}/transactions/queue',
+    42161: 'https://app.safe.global/arb1:{0}/transactions/queue'
 }
 
 class DelegateSafe(ApeSafe):
@@ -45,24 +35,12 @@ class DelegateSafe(ApeSafe):
         Create an ApeSafe from an address or a ENS name and use a default connection.
         """
         multisends[10] = "0x998739BFdAAdde7C933B942a68053933098f9EDa"
-        backend_urls = {
-            'gnosis': self.transaction_service.URLS_BY_NETWORK,
-            'yearn': {1: 'https://safe-transaction.mainnet.gnosis.yearn.tools/'}
-        }
 
-        # default to gnosis if we don't have a yearn version
-        if network.chain.id in backend_urls["gnosis"]:
-            if network.chain.id not in backend_urls[self.backend_type]:
-                backend_url_from_config = backend_urls["gnosis"][network.chain.id]
-                self.frontend_url = ["gnosis"][network.chain.id]
-            else:
-                backend_url_from_config = backend_urls[self.backend_type][network.chain.id]
-                self.frontend_url = gnosis_frontend_urls[self.backend_type][network.chain.id]
+        if network.chain.id in gnosis_frontend_urls:
+            self.frontend_url = gnosis_frontend_urls[network.chain.id]
         else:
-            backend_url_from_config = backend_urls["gnosis"][1]
-            self.frontend_url = gnosis_frontend_urls["gnosis"][1]
+            self.frontend_url = gnosis_frontend_urls[1]
 
-        self.base_url = base_url or backend_url_from_config
         super().__init__(address, base_url=self.base_url, multisend=multisend)
 
     @property
@@ -72,11 +50,6 @@ class DelegateSafe(ApeSafe):
     @property
     def is_send(self):
         return os.environ.get("GITHUB_ACTION_SEND", "").lower() == "true"
-
-    @property
-    def backend_type(self):
-        backend_from_env = os.environ.get("BE", "").lower()
-        return backend_from_env if backend_from_env != "" else "gnosis"
 
     def post_transaction(self, safe_tx: SafeTx):
         super().post_transaction(safe_tx)
