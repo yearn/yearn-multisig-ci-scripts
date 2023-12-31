53f9e224521720a96dcf64caf8d80edbbc92e5ea
kx9x <kx9x@protonmail.com>
2023-12-30 18:16:57 -0800

    fix: enable sentry tracing

--- multisig_ci/ci_override.py
+++ multisig_ci/ci_override.py
@@ -17,13 +17,9 @@ try:
     sentry_dsn = os.environ.get("SENTRY_DSN")
     sentry_sdk.init(
         dsn=sentry_dsn,
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
         traces_sample_rate=1.0,
-        # Set profiles_sample_rate to 1.0 to profile 100%
-        # of sampled transactions.
-        # We recommend adjusting this value in production.
         profiles_sample_rate=1.0,
+        enable_tracing=True,
     )
     print("Sentry initialized!")
 except Exception:
